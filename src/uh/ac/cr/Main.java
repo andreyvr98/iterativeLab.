cd97ba3b57c69a4a9ec2e213093f1d77bb56f0bb
andreycr98 <santiagovr3111@gmail.com>
2021-06-17 19:47:11 -0600

    Last commit.

--- src/uh/ac/cr/Main.java
+++ src/uh/ac/cr/Main.java
@@ -3,6 +3,7 @@ import uh.ac.cr.iterativeControl.IterativeControl;
 public class Main {
 
     public static void main(String[] args) {
+        //Se hace la creacion de un nuevo objeto y llamado de la clase.
         IterativeControl  iterativeControl =new IterativeControl();
         iterativeControl.control();
 
--- src/uh/ac/cr/iterativeControl/IterativeControl.java
+++ src/uh/ac/cr/iterativeControl/IterativeControl.java
@@ -12,32 +12,41 @@ public class IterativeControl {
 
     }
     public  void control (){
+        //Se utiliza un ciclo para que el usuraio realice el proseso hasta que el lo desee.
         do {
-            System.out.println("Por favor seleccione el requisito que desea utilizar.\nDigite del 1 al 3 para los requisitos y 4 para salir.");
+            System.out.println("Por favor selecione el requisito que desea utilizar.\nDigite del 1 al 2 para los requisitos y 3 para salir.");
             option = (int) scanner.nextFloat();
             int counter=0;
 
-            switch (option){
-                case 1:
-                    System.out.println("¿Cuántas veces desea imprimir “Hello world”? ");
-                    int quantity = scanner.nextInt();
-                    System.out.println("Bucle while\n");
-                    while (counter < quantity){
-                        System.out.println("Hello world.");
-                        counter++;
-                    }
-                    System.out.println("\nBucle for");
-                    for (counter = 0;counter < quantity; counter++){
-                        System.out.println("Hello world.");
-                    }
-                    break;
-                case 2:
-                    System.out.println("¿Cuántas veces desea imprimir “Hello world”? ");
-                    quantity = scanner.nextInt();
-                    do {
-                        System.out.println("Hello world.");
-                        counter++;
-                    }while (counter < quantity);
+            //Se procede a validar que la opción ingresada por el usuario sea correcta.
+            if (option <= 0 || option > 3 ){
+                System.out.println("Por favor ingrese una opción valida.");
+
+            }else {
+                switch (option) {
+                    case 1:
+                        System.out.println("¿Cuántas veces desea imprimir “Hello world”? ");
+                        int quantity = scanner.nextInt();
+                        System.out.println("Bucle while\n");
+                        while (counter < quantity) {
+                            System.out.println("Hello world.");
+                            counter++;
+                        }
+                        System.out.println("\nBucle for");
+                        for (counter = 0; counter < quantity; counter++) {
+                            System.out.println("Hello world.");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("¿Cuántas veces desea imprimir “Hello world”? ");
+                        quantity = scanner.nextInt();
+                        do {
+                            System.out.println("Hello world.");
+                            counter++;
+
+                        } while (counter < quantity);
+                        break;
+                }
             }
         }while (option < 4);
 
